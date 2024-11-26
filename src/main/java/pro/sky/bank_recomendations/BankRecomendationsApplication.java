08ce4b6a0bc9f1ad5de6f0ce7078584a3a519c82
Chowo <bob-xiii@rambler.ru>
2024-11-26 18:46:22 +0100

    Initial commit

---
+++ src/main/java/pro/sky/bank_recomendations/BankRecomendationsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.bank_recomendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRecomendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRecomendationsApplication.class, args);
+	}
+
+}
